448bf15f22852a12f5a81cb6392686bf553f3785
Chhavi <137093294+chhavii04@users.noreply.github.com>
2024-08-10 23:40:39 +0530

    Multiply two numbers represented by Linked Lists

--- Linked List/Level 2/Multiply2LLs.cpp
+++ Linked List/Level 2/Multiply2LLs.cpp
@@ -1,5 +1,3 @@
-const int MOD=1000000007;
-#include<math.h>
 /* Linked list node structure
 struct Node
 {
@@ -18,27 +16,10 @@ two  linked lists l1 and l2
 and returns their product*/
 
 /* Multiply contents of two linked lists */
+const int MOD=1e9+7;
 class solution {
 public:
-int digitcount(Node* head){
-    int digit=0;
-    while(head!=NULL){
-        int data=head->data;
-        while(data){
-            if(data==0){
-                digit++;
-            }else{
-                while (data > 0) {
-                    digit++;
-                    data /= 10;
-                }
-            }
-        }
-        head=head->next;
-    }
-    return digit;
-}
-long long Numberlls(Node* temp,int n){
+long long multiplyNum(Node* temp){
     long long sum=0;
     while(temp!=NULL){
         int data=temp->data;
@@ -48,9 +29,6 @@ long long Numberlls(Node* temp,int n){
     return sum;
 }
 long long multiplyTwoLists(Node *first, Node *second) {
-    int digit1=digitcount(first);
-    int digit2=digitcount(second);
-    long long x=(Numberlls(first,digit1)*Numberlls(second,digit2))%MOD;
-    return x;
+    return (multiplyNum(first)*multiplyNum(second))%MOD;
 }
 };
