c8271ead9704afce32abddbd5cefa8cc9aca53de
Chhavi <137093294+chhavii04@users.noreply.github.com>
2024-08-18 17:51:21 +0530

    String Manipulation(Delete consecutive same words)

---
+++ Stack/Level 2/RemoveConsecutiveSame.cpp
@@ -0,0 +1,15 @@
+// User function Template for C++
+class Solution {
+public:
+int removeConsecutiveSame(vector<string>& arr) {
+    stack<string> s;
+    for(int i=0;i<arr.size();i++){
+        if(!s.empty() && s.top()==arr[i]){
+            s.pop();
+        }else{
+            s.push(arr[i]);
+        }
+    }
+    return s.size();
+}
+};
